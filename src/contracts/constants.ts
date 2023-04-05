f7baecff7a01c9952fa32fb065a47c3e5834a5a5
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2023-04-05 17:31:44 +0100

    added:contracts

---
+++ src/contracts/constants.ts
@@ -0,0 +1,6 @@
+export const ARTIST_CONTRACT_ADDRESS=""
+export const ARTIST_ABI= []
+export const FACTORY_CONTRACT_ADDRESS=""
+export const FACTORY_ABI=[]
+export const MARKETPLACE_CONTRACT_ADDRESS =""
+export const MARKETPLACE_ABI=""
